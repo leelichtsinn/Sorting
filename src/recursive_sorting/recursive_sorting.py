2c9034954cafdf0136f821f8dda412aec3dbc75b
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-12-10 21:08:53 -0500

    added mid var for merge_sort, and divded arr into subarrays added while loop to evaluate arr elements and swap them.

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,15 +3,35 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    i, j = 0, 0
+    while (len(merged_arr) < len(arrA) + len(arrB)):
+        if arrA[i] < right[j]:
+            merged_arr.append(arrA[i])
+            i+=1
+        else:
+            merged_arr.append(arrB[j])
+            j+=1
+        if i == len(arrA) or len(arrB):
+            merged_arr.extend(arrA[i:] or arrB[j])
+            break
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) < 2:
+        return arr
 
-    return arr
+    # find middle of arr
+    mid = len(arr) // 2
+
+    # divide arr
+    left_arr = arr[:mid]
+    right_arr = arr[mid:]
+
+    return merge(left_arr, right_arr)
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +40,7 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
